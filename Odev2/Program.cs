4d94318981bbfe7f8e6e2695449c1d4d82e6f3cc
Toshiba <Toshiba@DESKTOP-6PE3TS6>
2023-08-25 19:00:10 +0300

    Add project files.

---
+++ Odev2/Program.cs
@@ -0,0 +1,32 @@
+﻿public abstract class sporcu
+{
+    public string adSoyad;
+    public string gorev;
+    public int lisansSuresi;
+    public int ucret;
+
+    public sporcu()
+    {
+        adSoyad = "Mauro Icardi";
+        gorev = "Futbolcu";
+        lisansSuresi = 2;
+        ucret = 10000000;
+    }
+}
+public class futbolcu : sporcu
+{
+    public futbolcu()
+    {
+        Console.WriteLine($"Futbolcu Özellikleri\nAdı Soyadı:{adSoyad}\nGörevi:{gorev}\nLisan Süresi:{lisansSuresi} yıl\nÜcret:{ucret}");
+
+    }
+}
+class ftblcu
+{
+    static void Main(string[] args)
+    {
+        futbolcu f1= new futbolcu();
+        Console.WriteLine();
+    }
+}
+    
